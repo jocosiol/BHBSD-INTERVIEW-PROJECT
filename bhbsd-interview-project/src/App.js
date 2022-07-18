8abdb60bb1ec2fe3300204c842ea841eb269f4d8
jocosiol <jocosiol@gmail.com>
2022-07-18 17:48:08 +0300

    Commit UI

--- bhbsd-interview-project/src/App.js
+++ bhbsd-interview-project/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from '@mui/material/Typography';
+import ClientCard from "./components/ClientCard"
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ flexGrow: 1}}>
+      <Grid sx={{textAlign: "center", marginBottom: 5 }}>
+        <Typography sx={{color: "white"}} variant="h4" gutterBottom component="div">
+          MEET OUR 30 FEATURED INVESTORS
+        </Typography>
+      </Grid>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        rowSpacing={4}
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        {Array.from(Array(30)).map((_, index) => (
+          <Grid item xs={2} sm={2.3} md={2.3} key={index}>
+            <ClientCard />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 }
 
---
+++ bhbsd-interview-project/src/components/ClientCard.js
@@ -0,0 +1,33 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import ButtonBase from "@mui/material/ButtonBase";
+
+function ClientCard() {
+  return (
+    <ButtonBase>
+      <Card sx={{ maxWidth: 200, backgroundColor: "black" }}>
+        <CardMedia
+          component="img"
+          height="200"
+          image="https://thumbs.dreamstime.com/b/default-avatar-profile-icon-social-media-user-vector-default-avatar-profile-icon-social-media-user-vector-portrait-176194876.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography sx={{color: "white"}} align="left" gutterBottom variant="h6" component="div">
+            Full Name
+          </Typography>
+          <Typography sx={{color: "red"}} align="left" variant="body2" color="text.secondary">
+            Client's Company
+          </Typography>
+        </CardContent>
+      </Card>
+    </ButtonBase>
+  );
+}
+
+export default ClientCard;
